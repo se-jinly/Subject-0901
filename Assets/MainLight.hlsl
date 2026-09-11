7415854a27755ee8f62fce78654defe109e455c6
se-jinly <sejinahn421@gmail.com>
2026-09-11 20:50:21 +0900

    2d
    gragh

--- Assets/MainLight.hlsl
+++ Assets/MainLight.hlsl
@@ -1,3 +1,5 @@
+#pragma multi_compile _ _MAIN_LIGHT_SHADOWS _MAIN_LIGHT_SHADOWS_CASCADE _MAIN_LIGHT_SHADOWS_SCREEN
+#pragma multi_compile _ _SHADOWS_SOFT
 void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float ShadowAtten)
 {
 #if SHADERGRAPH_PREVIEW
